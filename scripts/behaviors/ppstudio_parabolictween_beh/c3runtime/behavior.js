67afbed9f479b6334525330ba37efd8c1d2b16b0
Fikri <dev.fikri07@gmail.com>
2025-01-17 20:15:50 +0700

    1.6.9

---
+++ scripts/behaviors/ppstudio_parabolictween_beh/c3runtime/behavior.js
@@ -0,0 +1,11 @@
+"use strict";
+const C3 = globalThis.C3;
+{
+	C3.Behaviors.ppstudio_parabolictween_beh = class ppstudio_parabolictween_beh_behavior extends globalThis.ISDKBehaviorBase
+	{
+		constructor()
+		{
+			super();
+		}		
+	};
+}
--- scripts/c3main.js
+++ scripts/c3main.js
@@ -4,5 +4,7 @@ import "./../bonebuffer.js";
 import "./../objectbuffer.js";
 import "./../gltfmodel.js";
 import "./../gltfmodelw.js";
+import "./../parabolic.obf.js";
 import "./c3runtime.js";
+import "./behaviors/ppstudio_parabolictween_beh/c3runtime/main.js";
 import "./objRefTable.js";
